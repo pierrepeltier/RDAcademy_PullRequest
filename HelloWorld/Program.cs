050cfe092ae7e82f18b54b0120ecdf7a6921f6c3
Pierre Peltier <ppeltier@talentsoft.com>
2015-04-17 13:20:05 +0200

    #ninja fix maj

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            Console.WriteLine("Hello world!");
 
             // wait that user press a key before exit.
             Console.ReadKey(true);
